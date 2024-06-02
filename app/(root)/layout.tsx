cdde5991115d2b94fa1ec705967ae5e12f172105
aminulds <aminulds61@gmail.com>
2024-06-02 08:45:17 +0600

    sidebar done

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,11 @@
 import React, {ReactNode} from 'react';
+import Sidebar from "@/components/shared/sidebar";
 
 const RootLayout = ({children}: {children: ReactNode}) => {
     return (
         <main className="root">
+            <Sidebar />
+            {/*<MobileNav/>*/}
             <div className="root-container">
                 <div className="wrapper">
                     {children}
