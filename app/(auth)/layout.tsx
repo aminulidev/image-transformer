2198a7d6534ab825d87bf606392e9347a6adcaa6
aminulds <aminulds61@gmail.com>
2024-06-02 07:38:51 +0600

    authentication

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+import React, {ReactNode} from 'react';
+
+const AuthLayout = ({children}: {children: ReactNode}) => {
+    return (
+        <main className="auth">{children}</main>
+    );
+};
+
+export default AuthLayout;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,35 @@
-import type { Metadata } from "next";
-import { IBM_Plex_Sans } from "next/font/google";
+import type {Metadata} from "next";
+import {IBM_Plex_Sans} from "next/font/google";
 import "./globals.css";
 import {cn} from "@/lib/utils";
+import {ClerkProvider} from "@clerk/nextjs";
 
 const IBMPlex = IBM_Plex_Sans({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-  variable: "--font-ibm-plex"
+    subsets: ["latin"],
+    weight: ["400", "500", "600", "700"],
+    variable: "--font-ibm-plex"
 });
 
 export const metadata: Metadata = {
-  title: "Image Transformer",
-  description: "Transform your image with AI",
+    title: "Image Transformer",
+    description: "Transform your image with AI",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
-        {children}
-      </body>
-    </html>
-  );
+export default function RootLayout(
+    {
+        children,
+    }: Readonly<{
+        children: React.ReactNode;
+    }>) {
+    return (
+        <ClerkProvider appearance={{
+            variables: {colorPrimary: "#624cfa"}
+        }}>
+            <html lang="en">
+            <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+            {children}
+            </body>
+            </html>
+        </ClerkProvider>
+    );
 }
